e61820fc6c855bb3e3034a75dfaadd51854a22cc
aryanphulkar90 <aryanphulkar1@gmail.com>
2025-08-20 21:44:06 +0530

    adding login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Routes, Route, BrowserRouter } from "react-router";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile"
+import Feed from "./components/Feed";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/feed" element={<Feed/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
 }
 
 export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import EmailInputField from "./EmailInputField";
+import PasswordInputField from "./PasswordInputField";
+import axios from "axios";
+import { isEmail } from "validator";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { baseURL } from "../utils/constansts";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const handleSubmit = async () => {
+    try {
+      if (!isEmail(email)) {
+        throw new Error("Email is not valid");
+      }
+      const res = await axios.post(
+        `${baseURL}/login`,
+        {
+          emailID: email,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(addUser(res.data.data));
+      navigate("/feed")
+    } catch (err) {
+      console.log(err);
+      setError(err.message);
+    }
+  };
+  return (
+    <div className="flex justify-center items-center my-10">
+      <div className="card w-96 shadow-sm bg-base-300">
+        <div className="card-body">
+          <div className="card-title justify-center my-4">
+            <h2>Login</h2>
+          </div>
+          <EmailInputField setEmail={setEmail} />
+          <PasswordInputField setPassword={setPassword} />
+          <div className="flex justify-center py-1">
+            <h4>{error}</h4>
+          </div>
+          <div className="card-actions justify-center py-1">
+            <button className="btn btn-primary" onClick={handleSubmit}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
